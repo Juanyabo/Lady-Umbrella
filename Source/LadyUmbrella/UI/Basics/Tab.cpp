86e27b83f3ffa4dd51f3337fd4805791b720aecb
JuanGianella <juan.gianella19@gmail.com>
2025-12-05 16:04:16 +0100

    Replaced source file with version 1.1

--- Source/LadyUmbrella/UI/Basics/Tab.cpp
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-#include "Tab.h"
-
-FText UTab::GetText() const { return Text->GetText(); }
-void UTab::SetText(FText NewText) { Text->SetText(NewText); }
-void UTab::UpdateTab() const { Switcher->SetActiveWidgetIndex(bActive ? 1 : 0); }
